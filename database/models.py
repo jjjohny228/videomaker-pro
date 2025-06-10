3d0c073ede0003b696f5e28c3aab166bbad022d8
jjjohny228 <gggleb13@gmail.com>
2025-06-10 23:13:07 +0300

    Added SourceVideos model

--- database/models.py
+++ database/models.py
@@ -279,72 +279,21 @@ class ApiKey(_BaseModel):
     class Meta:
         table_name = 'api_keys'
 
-#
-# class JobStatus(Enum):
-#     QUEUED = "queued"
-#     PROCESSING = "processing"
-#     COMPLETED = "completed"
-#     FAILED = "failed"
-#
-#
-# class Job:
-#     def __init__(self, job_id: str, title: str, script: str, brand_kit_id: str):
-#         self.job_id = job_id
-#         self.title = title
-#         self.script = script
-#         self.brand_kit_id = brand_kit_id
-#         self.status = JobStatus.QUEUED
-#         self.progress = 0
-#         self.message = "Задача в очереди"
-#         self.result = None
-#         self.error = None
-#         self.created_at = datetime.now()
-#         self.updated_at = datetime.now()
-#         self.completed_at = None
-#
-#     def update_progress(self, progress: int, message: str):
-#         """Обновляет прогресс выполнения задачи"""
-#         self.progress = progress
-#         self.message = message
-#         self.updated_at = datetime.now()
-#
-#     def mark_as_processing(self):
-#         """Отмечает задачу как обрабатываемую"""
-#         self.status = JobStatus.PROCESSING
-#         self.message = "Задача обрабатывается"
-#         self.updated_at = datetime.now()
-#
-#     def mark_as_completed(self, result: str):
-#         """Отмечает задачу как завершенную"""
-#         self.status = JobStatus.COMPLETED
-#         self.progress = 100
-#         self.message = "Задача завершена"
-#         self.result = result
-#         self.updated_at = datetime.now()
-#         self.completed_at = datetime.now()
-#
-#     def mark_as_failed(self, error: str):
-#         """Отмечает задачу как неудачную"""
-#         self.status = JobStatus.FAILED
-#         self.message = f"Ошибка: {error}"
-#         self.error = error
-#         self.updated_at = datetime.now()
-#
-#     def to_dict(self) -> Dict[str, Any]:
-#         """Преобразует объект в словарь"""
-#         return {
-#             "job_id": self.job_id,
-#             "title": self.title,
-#             "brand_kit_id": self.brand_kit_id,
-#             "status": self.status.value,
-#             "progress": self.progress,
-#             "message": self.message,
-#             "result": self.result,
-#             "error": self.error,
-#             "created_at": self.created_at.isoformat(),
-#             "updated_at": self.updated_at.isoformat(),
-#             "completed_at": self.completed_at.isoformat() if self.completed_at else None
-#         }
+class SourceVideos(_BaseModel):
+    """
+    This model stores videos for the brand kit.
+    """
+    brand_kit = pw.ForeignKeyField(BrandKit, backref='source_videos',
+                                   help_text="The BrandKit for which this videos is selected.")
+    path = pw.CharField(constraints=[pw.Check("avatar_clip_path LIKE '%.mp4' OR avatar_clip_path LIKE '%.mov' "
+                                              "OR avatar_clip_path LIKE '%.avi' OR avatar_clip_path LIKE '%.mkv' "
+                                              "OR avatar_clip_path IS NULL")
+                                     ],
+                        help_text="Path to a video")
+
+    class Meta:
+        table_name = 'source_videos'
+
 
 # --- DB Initialization Utility ---
 def register_models() -> None:
