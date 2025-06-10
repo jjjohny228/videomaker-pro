2f23ba2675864dad7263116ebd40dfb3c03132ce
jjjohny228 <gggleb13@gmail.com>
2025-06-10 22:48:34 +0300

    Changed config

--- core/config.py
+++ core/config.py
@@ -4,20 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    BACKGROUND_VIDEO_FOLDER = 'resource/background_videos'
-    BACKGROUND_IMAGE_FOLDER = 'resource/background_images'
-    COMMENTATOR = 'resource/chromakey_backgrounds/commentator.mp4'
-    BACKGROUND_VIDEO = 'resource/background_videos/minecraft2.mov'
-    BOTTOM_VIDEOS = 'resource/bottom_videos'
-    BACKGROUND_IMAGE = 'resource/background_images/black.png'
-
     SOURCE_FOLDER = 'source'
     RESULT_FOLDER = 'result'
     TEMP_FOLDER = 'temp'
 
     OUTPUT_PTS = 30
-
-    COMMENTATOR_COLOR = '0x05c803'
 
     # Improves rendering
     if sys.platform == 'darwin':
